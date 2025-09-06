ed2b45f5d072725a01bd9c6ce0dbfc7e773cce9e
Ashmit <ashmitv704@gmail.com>
2025-09-07 01:59:31 +0530

    Added a theme loader to prevent the flash of unstyled content

---
+++ theme-loader.js
@@ -0,0 +1,10 @@
+// This script runs before the page is rendered to prevent the "flash"
+// when switching between dark and light themes.
+
+(function() {
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+})();
